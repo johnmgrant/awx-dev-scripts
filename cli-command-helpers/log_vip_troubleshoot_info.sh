647f33cbc8329e895bec37bc1011aec1302de2f8
John Grant <john.grant@accuweather.com>
2021-11-23 18:21:39 -0500

    Improve vip dev-env troubleshooting script - Add missing commands requested for ticket follow-up - Improve logging by adding newlines and echos for the running commands

--- cli-command-helpers/log_vip_troubleshoot_info.sh
+++ cli-command-helpers/log_vip_troubleshoot_info.sh
@@ -3,7 +3,7 @@
 # Script to Log VIP dev-env information for troubleshooting:
 #
 # EXAMPLE:
-#   - 
+#   -
 #
 if [ $# -lt 1 ]; then
 	echo "usage: $0 <env-name> [is-slug]"
@@ -14,18 +14,37 @@ ENV_NAME=$1
 IS_SLUG=${2-false}
 
 perform_log() {
+	echo -e "- vip cli version:"
 	vip -v
 
 	if [[ -z "$IS_SLUG" || "$IS_SLUG" = false ]]; then
+		echo -e "\n- vip @$ENV_NAME dev-env info:"
 		vip @"$ENV_NAME" dev-env info
 	else
+		echo -e "\n- vip --slug=$ENV_NAME dev-env info:"
 		vip --slug="$ENV_NAME" dev-env info
 	fi
-	
-	lando wp eval 'var_dump(EP_HOST);' 
+
+	echo -e "\n- lando wp eval 'var_dump(EP_HOST);':"
+	lando wp eval 'var_dump(EP_HOST);'
+
+	echo -e "\n- lando wp vip-search status:"
 	lando wp vip-search status
+
+	echo -e "\n- lando info:"
 	lando info
+
+	echo -e "\n- docker container ls:"
 	docker container ls
+
+	echo -e "\n- docker container inspect ${ENV_NAME//[.-]}_vip-search_1:"
+	docker container inspect "${ENV_NAME//[.-]}_vip-search_1"
+
+	echo -e "\n- lando ssh -s vip-search --user root --command 'curl http://localhost:9200':"
+	lando ssh -s vip-search --user root --command "curl http://localhost:9200"
+
+	echo -e "\n- lando ssh -s vip-search --user root --command curl 'http://localhost:9200/_cluster/health?pretty':"
+	lando ssh -s vip-search --user root --command "curl http://localhost:9200/_cluster/health?pretty"
 }
 
 log_vip_env_troubleshoot_info() {
@@ -41,7 +60,7 @@ log_vip_env_troubleshoot_info() {
 		cd "$VIP_DEV_ENV_DIR/$ENV_NAME"
 	fi
 
-	perform_log 2>&1 | tee -a "$HOME/Desktop/vip-dev-env.log"
+	perform_log 2>&1 | tee -a "$HOME/Desktop/vip_dev-env_$ENV_NAME.log"
 
 	# Change back to previous working directory.
 	cd $CURRENT_DIR
